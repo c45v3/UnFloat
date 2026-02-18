858631e4bd6814f94c05fa09bc48168c84498bab
Casue <yqxf168@gmail.com>
2026-02-19 04:45:35 +0800

    Create MainHook.java

---
+++ app/src/main/java/io/c4/UnFloat/MainHook.java
@@ -0,0 +1,30 @@
+package io.c4.UnFloat;
+
+import android.view.View;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.processName.equals(lpparam.packageName)) return;
+
+        XposedHelpers.findAndHookMethod(View.class, "setVisibility", int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                View v = (View) param.thisObject;
+                if (isTarget(v)) {
+                    v.setAlpha(0f);
+                }
+            }
+        });
+    }
+
+    private boolean isTarget(View v) {
+        String name = v.getClass().getName();
+        return name.contains("FloatViewAbove") || name.contains("easyfloat");
+    }
+}
