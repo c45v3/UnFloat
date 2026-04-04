ddc6e1d491a9a52f526a02563c438dde7805f844
Casue <yqxf168@gmail.com>
2026-04-05 01:03:07 +0800

    Unhook visibility hook after first target match

--- app/src/main/java/io/c4/UnFloat/MainHook.java
+++ app/src/main/java/io/c4/UnFloat/MainHook.java
@@ -1,27 +1,25 @@
 package io.c4.UnFloat;
 
 import android.view.View;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook.Unhook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class MainHook implements IXposedHookLoadPackage {
-private static boolean isIntercepted = false;
-
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-       if (isIntercepted) return;
        if (!lpparam.processName.equals(lpparam.packageName)) return;
 
-        XposedHelpers.findAndHookMethod(View.class, "setVisibility", int.class, new XC_MethodHook() {
+        final Unhook[] unhookRef = new Unhook[1];
+        unhookRef[0] = XposedHelpers.findAndHookMethod(View.class, "setVisibility", int.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 View v = (View) param.thisObject;
                 if (isTarget(v)) {
                     v.setAlpha(0f);
-                    isIntercepted = true;                     
+                    unhookRef[0].unhook();
                 }
             }
         });
